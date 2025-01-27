0365852ae8433dbb36450179e6d7955b5083f3ac
tangerine238 <stephentang238@gmail.com>
2025-01-26 21:39:19 -0500

    pre-cut

---
+++ Assets/Scripts/CuttingCounter.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class CuttingCounter : BaseCounter
+{
+    public override void Interact(Player player)
+    {
+        if (!HasKitchenObject())
+        {
+            if (player.HasKitchenObject())
+            {
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            if (player.HasKitchenObject())
+            {
+
+            }
+            else
+            {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+
+}
