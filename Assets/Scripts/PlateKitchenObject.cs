613d36b7728cc4c040890e7e1bddf3c82bb838e9
tangerine238 <stephentang238@gmail.com>
2025-01-30 21:01:39 -0500

    plate recipes

---
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class PlateKitchenObject : KitchenObject
+{
+
+    [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
+    private List<KitchenObjectSO> kitchenObjectSOList;
+
+    private void Awake()
+    {
+        kitchenObjectSOList = new List<KitchenObjectSO>();
+    }
+
+    public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
+    {
+        if (!validKitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            return false;
+        }
+        if (kitchenObjectSOList.Contains(kitchenObjectSO))
+        {
+            return false;
+        }
+        else
+        {
+            kitchenObjectSOList.Add(kitchenObjectSO);
+            return true;
+        }
+
+    }
+}
