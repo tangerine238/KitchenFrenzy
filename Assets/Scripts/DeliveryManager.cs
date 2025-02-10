85b85022c8b14774ec3b1c7c19c0897ad7b67921
tangerine238 <stephentang238@gmail.com>
2025-02-10 15:48:36 -0500

    recipe ordering

---
+++ Assets/Scripts/DeliveryManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class DeliveryManager : MonoBehaviour
+{
+    [SerializeField] private RecipeListSO recipeListSO;
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 4;
+
+    private void Awake()
+    {
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update()
+    {
+        spawnRecipeTimer -= Time.deltaTime;
+        if (spawnRecipeTimer <= 0f)
+        {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+            if (waitingRecipeSOList.Count < waitingRecipesMax)
+            {
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0, recipeListSO.recipeSOList.Count)];
+                Debug.Log(waitingRecipeSO.recipeName);
+                waitingRecipeSOList.Add(waitingRecipeSO);
+            }
+        }
+    }
+
+}
---
+++ Assets/Scripts/ScriptableObject/RecipeListSO.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+//[CreateAssetMenu()]
+public class RecipeListSO : ScriptableObject
+{
+    public List<RecipeSO> recipeSOList;
+}
